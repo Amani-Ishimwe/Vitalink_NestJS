17f5568e7b30b05eb64f84646289f20265bcbf2b
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-08-16 21:48:24 +0300

    added the data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,28 @@ import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({isGlobal : true}),
-  UsersModule],
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

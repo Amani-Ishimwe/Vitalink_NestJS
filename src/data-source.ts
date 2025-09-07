9671ce1e713e0027e968c12a877bfc8de28600b1
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-07 20:07:44 +0300

    modifying the datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,15 @@
 import { DataSource } from "typeorm";
 import { User } from "./entities/user.entity";
 import { Patient } from "./entities/patient.entity";
+import { Doctor } from "./entities/doctor.entity";
+import { Receptionist } from "./entities/reception.entity";
+import { Department } from "./entities/department.entity";
+import { Appointment } from "./entities/appointment.entity";
+import { Prescription } from "./entities/prescription.entity";
+import { Ward } from "./entities/ward.entity";
+import { RoomAssign } from "./entities/roomAssign.entity";
+import { ShiftSchedule } from "./entities/shiftSchedule.entity";
+import { Bill } from "./entities/bill.entity";
 
  const AppDataSource = new DataSource({
     type:"postgres",
@@ -10,7 +19,18 @@ import { Patient } from "./entities/patient.entity";
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: false,
-    entities: [User, Patient],
+    entities: [User, 
+        Patient, 
+        Doctor, 
+        Receptionist, 
+        Department, 
+        Appointment, 
+        Prescription,
+        Ward,
+        RoomAssign,
+        ShiftSchedule,
+        Bill
+    ],
     logging: ["error","warn"]
 })
 

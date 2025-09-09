5f244adeb527391bc12d7fe4f08997cd7c3a4008
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-09 19:24:26 +0300

    working on user api endpoints

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from 'src/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+      @InjectRepository(User)
+      private readonly userRepository: Repository<User>
+
+  async create(
+    createUserDto: CreateUserDto
+  ):Promise<{ user: User}> {
+    const user = await this.userRepository.findOneBy({ email: createUserDto.email })
+    if (user){
+      throw new Error('User already exists')
+    }
+    const newUser = this.userRepository.create(createUserDto)
+    const savedUser = await this.userRepository.save(newUser)
+    return { user: savedUser}
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<{ users: User[] }> {
+    return this.userRepository.find().then(users => ({ users }))
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string): Promise<{ user: User }> {
+    return this.userRepository.findOneBy({ id: id }).then(user => {
+      if (!user) {
+        throw new Error('User not found')
+      }
+      return { user }
+    })
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {

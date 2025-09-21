574392c48b56129202a06e0bb64146de1a6cd19e
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-21 12:53:35 +0300

    sanitized the schedules side

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -1,37 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes, UseGuards } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import { Roles } from 'src/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
+import { Role } from 'src/entities/user.entity';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
 
 @Controller('schedules')
 export class SchedulesController {
   constructor(private readonly schedulesService: SchedulesService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Get()
   findAll() {
     return this.schedulesService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.schedulesService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() dto: CreateScheduleDto) {
     return this.schedulesService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Patch(":id")
   update(@Param("id") id: string, @Body() dto: UpdateScheduleDto) {
     return this.schedulesService.update(id, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Delete(":id")
   remove(@Param("id") id: string) {

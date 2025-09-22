ba4ed8d45def73be1e88689f41c51c641da8f2c8
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-22 20:28:20 +0300

    added exception filter logging

---
+++ src/filters/exception.filter.ts
@@ -0,0 +1,17 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Response  } from "express";
+
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter{
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse<Response>()  
+        const status = exception instanceof HttpException ? exception.getStatus() : 500; 
+
+        response.status(status).json({
+            statusCode: status,
+            message : exception instanceof Error ? exception.message: "Internal Server Error"
+        })
+    }
+}

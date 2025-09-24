52240656dfef735c222f133310e109584e4e5cae
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-24 10:48:52 +0300

    made the swagger documentation for prescriptions

--- src/prescriptions/prescriptions.controller.ts
+++ src/prescriptions/prescriptions.controller.ts
@@ -5,8 +5,11 @@ import { UpdatePrescriptionDto } from './dto/update-prescription.dto';
 import { Roles } from 'src/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('prescriptions')
+@ApiTags("Prescriptions")
+@ApiBearerAuth()
 export class PrescriptionsController {
   constructor(private readonly prescriptionsService: PrescriptionsService) {}
 
@@ -14,6 +17,8 @@ export class PrescriptionsController {
   @Roles('DOCTOR')
   @Post()
   @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: "Creating Prescription"})
+  @ApiResponse({ status: 201, description: "Prescription Created Successfully"})
   create(@Body() createPrescriptionDto: CreatePrescriptionDto) {
     return this.prescriptionsService.create(createPrescriptionDto);
   }
@@ -21,6 +26,8 @@ export class PrescriptionsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('DOCTOR')
   @Get()
+  @ApiOperation({ summary: " Fetching Prescriptions "})
+  @ApiResponse({ status:200, description: "Prescriptions fetched successfully"})
   findAll() {
     return this.prescriptionsService.findAll();
   }
@@ -28,6 +35,8 @@ export class PrescriptionsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('DOCTOR')
   @Get(':id')
+  @ApiOperation({ summary: "Fetching single prescription"})
+  @ApiResponse({ status: 200, description: "Prescription fetched successfully"})
   findOne(@Param('id') id: string) {
     return this.prescriptionsService.findOne(id);
   }
@@ -35,6 +44,8 @@ export class PrescriptionsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('DOCTOR')
   @Patch(':id')
+  @ApiOperation({ summary: "Updating single prescription"})
+  @ApiResponse({ status: 200, description: "Prescription updated successfully"})
   update(@Param('id') id: string, @Body() updatePrescriptionDto: UpdatePrescriptionDto) {
     return this.prescriptionsService.update(id, updatePrescriptionDto);
   }
@@ -42,6 +53,8 @@ export class PrescriptionsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('DOCTOR')
   @Delete(':id')
+  @ApiOperation({ summary: "Deleting single prescription"})
+  @ApiResponse({ status: 200, description: "Prescription deleted successfully"})
   remove(@Param('id') id: string) {
     return this.prescriptionsService.remove(id);
   }

2f537a0b4ac82044f270670731a86101a188f925
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-09-24 10:43:27 +0300

    made the swagger documentation for patients

--- src/doctors/doctors.controller.ts
+++ src/doctors/doctors.controller.ts
@@ -5,8 +5,11 @@ import { UpdateDoctorDto } from './dto/update-doctor.dto';
 import { Roles } from 'src/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('doctors')
+@ApiTags("Doctors")
+@ApiBearerAuth()
 export class DoctorsController {
   constructor(private readonly doctorsService: DoctorsService) {}
 
@@ -14,6 +17,8 @@ export class DoctorsController {
   @Roles('ADMIN')
   @Post()
   @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: "Creating a new doctor"})
+  @ApiResponse({ status: 201, description : "New Doctor Created Successfully"})
   create(@Body() createDoctorDto: CreateDoctorDto) {
     return this.doctorsService.create(createDoctorDto);
   }
@@ -21,6 +26,10 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Get()
+  @ApiOperation({ summary : "Fetching all doctors"})
+  @ApiResponse({ status :200, description: "Fetched All Doctors Successfully"})
+  @ApiQuery({ name :'page', required: false, type : Number, description:"Page number (default : 1)" })
+  @ApiQuery({ name :'limit', required: false, type : Number, description:"Items per page (default : 10)" })
   findAll(
    @Query('page') page: number = 1,
    @Query('limit') limit: number = 10) {
@@ -31,6 +40,8 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN','DOCTOR')
   @Get(':id')
+  @ApiOperation({summary : "Fetch single doctor" })
+  @ApiResponse({ status:200, description:" Doctor fetched successfully"})
   findOne(@Param('id') id: string) {
     return this.doctorsService.findOne(id);
   }
@@ -38,6 +49,8 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Get(':id/appointments')
+  @ApiOperation({ summary: "Fetch appointments"})
+  @ApiResponse({ status: 200, description: "Appointments fetched successfully"})
   fetchAppointments(@Param('id') id: string) {
     return this.doctorsService.fetchAppointments(id);
   }
@@ -45,6 +58,8 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'DOCTOR')
   @Get(':id/schedule')
+  @ApiOperation({ summary :"Fetching schedules"})
+  @ApiResponse({ status: 200, description: "Fetch schedules successfully"})
   fetchSchedule(@Param('id') id: string) {
     return this.doctorsService.fetchSchedule(id);
   }
@@ -52,6 +67,8 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Patch(':id')
+  @ApiOperation({ summary: "Updating a doctor"})
+  @ApiResponse({ status: 200, description: "Doctor Updated successfully"})
   update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto) {
     return this.doctorsService.update(id, updateDoctorDto);
   }
@@ -59,6 +76,8 @@ export class DoctorsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Delete(':id')
+  @ApiOperation({ summary: "Deleting  A Doctor"})
+  @ApiResponse({ status: 200, description: "Doctor Deleted Successfully"})
   remove(@Param('id') id: string) {
     return this.doctorsService.remove(id);
   }
--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -5,8 +5,11 @@ import { UpdatePatientDto } from './dto/update-patient.dto';
 import { Roles } from 'src/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('patients')
+@ApiTags("Patients")
+@ApiBearerAuth()
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
@@ -14,6 +17,8 @@ export class PatientsController {
   @Roles('ADMIN','RECEPTIONIST')
   @Post()
   @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary : "Creating a new patient"})
+  @ApiResponse({ status: 201 , description: "New patient created Successfully"})
   create(@Body() createPatientDto: CreatePatientDto) {
     return this.patientsService.create(createPatientDto);
   }
@@ -21,9 +26,13 @@ export class PatientsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN','RECEPTIONIST')
   @Get()
+  @ApiOperation({ summary: "Fetching patients"})
+  @ApiResponse({ status: 200, description: "Patients fetched successfully"})
+  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number (default: 1)' })
+  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Items per page (default: 10)' })
   findAll(
     @Query('page') page: number = 1,
-    @Query('limit') limit: number = 20
+    @Query('limit') limit: number = 10
   ) {
     return this.patientsService.findAll(page, limit);
   }
@@ -31,6 +40,8 @@ export class PatientsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN','RECEPTIONIST','PATIENT')
   @Get(':id')
+  @ApiOperation({ summary : "Fetching a patient successfully"})
+  @ApiResponse({ status : 200, description: "Patient fetched successfully"})
   findOne(@Param('id') id: string) {
     return this.patientsService.findOne(id);
   }
@@ -38,6 +49,8 @@ export class PatientsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN','RECEPTIONIST')
   @Patch(':id')
+  @ApiOperation({ summary : "Updating a patient record"})
+  @ApiResponse({ status: 200, description : "Patient Updated Successfully"})
   update(@Param('id') id: string, @Body() updatePatientDto: UpdatePatientDto) {
     return this.patientsService.update(id, updatePatientDto);
   }
@@ -45,6 +58,8 @@ export class PatientsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN','RECEPTIONIST')
   @Delete(':id')
+  @ApiOperation({ summary: "Deleting a patient record"})
+  @ApiResponse({ status: 200, description : "Patient Deleted Successfully"})
   remove(@Param('id') id: string) {
     return this.patientsService.remove(id);
   }
